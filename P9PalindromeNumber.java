61dcab18f52951049bdbac21dca24152bd28f3ff
prakhar-thecoder <pnp14072005@gmail.com>
2024-07-02 02:23:56 +0530

    Added problems 1 and 9

---
+++ P9PalindromeNumber.java
@@ -0,0 +1,13 @@
+public class P9PalindromeNumber {
+    public static void main(String[] args) {
+        System.out.println(isPalindrome(121));
+    }
+    public static boolean isPalindrome(int x) {
+        String old = String.valueOf(x);
+        String rev = "";
+        for (int i = old.length() - 1; i >= 0; i--) {
+            rev += old.charAt(i);
+        }
+        return old.equals(rev);
+    }
+}
