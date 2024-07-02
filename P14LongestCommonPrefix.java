9efc148f7064b0de4f8829095fb14b92acc9c569
prakhar-thecoder <pnp14072005@gmail.com>
2024-07-02 19:05:21 +0530

    Added problem 14

---
+++ P14LongestCommonPrefix.java
@@ -0,0 +1,25 @@
+public class P14LongestCommonPrefix {
+    public static void main(String[] args) {
+        System.out.println("Result: " + longestCommonPrefix(new String[]{
+                "dog", "racecar", "cat"
+        }));
+    }
+    public static String longestCommonPrefix(String[] strs) {
+        String result = "";
+        int lowestLength = strs[0].length();
+        for (String s: strs) {
+            if (s.length() < lowestLength)
+                lowestLength = s.length();
+        }
+
+        for (int i = 0; i < lowestLength; i++) {
+            char c = strs[0].charAt(i);
+            for (String s: strs) {
+                if (s.charAt(i) != c)
+                    return result;
+            }
+            result += c;
+        }
+        return result;
+    }
+}
