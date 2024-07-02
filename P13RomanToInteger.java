f14709044871785e03b4ccbfac3f3f8bfa5b536a
prakhar-thecoder <pnp14072005@gmail.com>
2024-07-02 18:44:22 +0530

    Added problem 13

---
+++ P13RomanToInteger.java
@@ -0,0 +1,33 @@
+import java.util.HashMap;
+
+public class P13RomanToInteger {
+    public static void main(String[] args) {
+        System.out.println("Result: " + romanToInt("LVIII"));
+    }
+    public static int romanToInt(String s) {
+        HashMap<Character, Integer> map = new HashMap<>();
+        map.put('I', 1);
+        map.put('V', 5);
+        map.put('X', 10);
+        map.put('L', 50);
+        map.put('C', 100);
+        map.put('D', 500);
+        map.put('M', 1000);
+        int result = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            if (i < s.length() - 1) {
+                if (map.get(s.charAt(i+1)) > map.get(s.charAt(i))) {
+                    result += (map.get(s.charAt(i+1)) - map.get(s.charAt(i)));
+                    i += 1;
+                } else {
+                    result += map.get(s.charAt(i));
+                }
+            } else {
+                result += map.get(s.charAt(i));
+            }
+        }
+
+        return result;
+    }
+}
