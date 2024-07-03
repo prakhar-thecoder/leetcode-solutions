525e088ae130bf2b3853d6e92922bbf5e79ed5bc
prakhar-thecoder <pnp14072005@gmail.com>
2024-07-03 12:16:05 +0530

    Added problem 20

---
+++ P20ValidParantheses.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+public class P20ValidParantheses {
+    public static void main(String[] args) {
+        System.out.println("Result: " + isValid("["));
+    }
+    public static boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        ArrayList<Character> openBrackets = new ArrayList<>(
+                List.of(new Character[]{'(', '{', '['})
+        );
+        ArrayList<Character> closeBrackets = new ArrayList<>(
+                List.of(new Character[]{')', '}', ']'})
+        );
+        for (int i = 0; i < s.length(); i++) {
+            if (openBrackets.contains(s.charAt(i))) {
+                stack.push(s.charAt(i));
+            } else {
+                if (stack.empty())
+                    return false;
+                if (closeBrackets.indexOf(s.charAt(i)) != openBrackets.indexOf(stack.pop())) {
+                    return false;
+                }
+            }
+        }
+        return stack.empty();
+    }
+}
