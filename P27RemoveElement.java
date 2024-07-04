88d4988fe5e93c9f0e850e6a864f541e3e0e8264
prakhar-thecoder <pnp14072005@gmail.com>
2024-07-05 01:18:48 +0530

    Added problem 27

---
+++ P27RemoveElement.java
@@ -0,0 +1,12 @@
+public class P27RemoveElement {
+    public int removeElement(int[] nums, int val) {
+        int c = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != val) {
+                nums[c] = nums[i];
+                c++;
+            }
+        }
+        return c;
+    }
+}
