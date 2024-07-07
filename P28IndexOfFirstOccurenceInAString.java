bc3aae5d4050219a2371fd66053c2a9640653b60
prakhar-thecoder <pnp14072005@gmail.com>
2024-07-07 13:21:17 +0530

    Added problem 28

---
+++ P28IndexOfFirstOccurenceInAString.java
@@ -0,0 +1,23 @@
+public class P28IndexOfFirstOccurenceInAString {
+    public int strStr(String haystack, String needle) {
+        for(int i = 0; i < haystack.length(); i++) {
+            if (haystack.charAt(i) == needle.charAt(0)) {
+                boolean found = true;
+                try {
+                    for (int temp = i, j = 0; j < needle.length(); temp++, j++) {
+                        if (haystack.charAt(temp) != needle.charAt(j)) {
+                            found = false;
+                            break;
+                        }
+                    }
+                } catch(StringIndexOutOfBoundsException e) {
+                    found = false;
+                }
+                if (found) {
+                    return i;
+                }
+            }
+        }
+        return -1;
+    }
+}
