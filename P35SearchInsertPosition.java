1229b03d7fa3547715417ae2667b90cecc1baf4f
prakhar-thecoder <pnp14072005@gmail.com>
2024-07-09 11:42:35 +0530

    Added problem 35

---
+++ P35SearchInsertPosition.java
@@ -0,0 +1,22 @@
+public class P35SearchInsertPosition {
+    public int searchInsert(int[] nums, int target) {
+        if (nums.length == 1) {
+            if (nums[0] > target || nums[0] == target) {
+                return 0;
+            } else {
+                return 1;
+            }
+        }
+
+        int i;
+        for (i = 0; i < nums.length - 1; i++) {
+            if (nums[i] == target || nums[i] > target) {
+                return i;
+            }
+            if (nums[i+1] == target || nums[i+1] > target) {
+                return i+1;
+            }
+        }
+        return i+1;
+    }
+}
